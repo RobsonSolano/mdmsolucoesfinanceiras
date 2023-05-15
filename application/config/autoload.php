bbc1d5a2c2719fe64056f335874ea5f25916cbdf
robson.solano <robson.solano@nanoincub.com.br>
2023-05-15 10:00:21 -0300

    Add alguns ajustes/melhorias para V1 do projeto

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'form', 'utility', 'upload_imagem');
+$autoload['helper'] = array('url', 'file', 'form', 'utility');
 
 /*
 | -------------------------------------------------------------------
